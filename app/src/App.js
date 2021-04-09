0cfa96168b48d6cf120a9a20cc32a19d5c603c2e
Alina <tyalinav@yandex.ru>
2021-04-10 01:39:52 +0300

    Создан элемент для представления toDoList

--- app/src/App.js
+++ app/src/App.js
@@ -1,12 +1,41 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { useState} from 'react'
+import InputTask from '../src/elements/InputTask'
+import Paper from '@material-ui/core/Paper';
+import Typography from '@material-ui/core/Typography';
+import { makeStyles } from '@material-ui/core/styles';
 
-function App() {
-  return (
-    <div className="App">
-      
-    </div>
-  );
+const useStyles = makeStyles((theme) => ({
+    root: {
+        display: 'flex',
+        flexWrap: 'wrap',
+        height: '100%'
+    },
+    paper: {
+        width: 'max-content',
+        height: 'max-content',
+        margin: 'auto',
+        padding: theme.spacing(4)
+    },
+    text: {
+        margin: 'auto'
+    }
+}));
+
+export default function ToDoApp() {
+    const classes = useStyles();
+    const [tasks, setTasks] = useState([])
+
+    async function handleSubmit(input) {
+      console.log(input)
+    }
+
+    return (
+        <div className={classes.root}>
+            <Paper elevation={5} className={classes.paper}>
+                <Typography align="center" variant="h6" gutterBottom="true">TO DO LIST</Typography>
+                <InputTask handleSubmit={handleSubmit} />
+            </Paper>
+        </div>
+    );
 }
-
-export default App;
